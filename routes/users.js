4298c3c554955a1ab7d25bff4d104b6c580e9248
ludike23 <ludike23@gmail.com>
2019-05-28 15:02:10 +0100

    e

--- routes/users.js
+++ routes/users.js
@@ -5,19 +5,19 @@ var mysql = require('mysql');
 const bcrypt = require('bcrypt');
 const saltRounds = 10;
 
-var connection = mysql.createConnection({
-    host: '127.0.0.1',
-    user: 'root',
-    password: '',
-    database: 'lawbook'
-
- });
 //var connection = mysql.createConnection({
-//    host: 'db4free.net',
-//    user: 'law_book',
-//    password: 'law_book',
-//    database: 'law_book'
-//});
+//    host: '127.0.0.1',
+//    user: 'root',
+//    password: '',
+//    database: 'lawbook'
+
+// });
+var connection = mysql.createConnection({
+    host: 'db4free.net',
+    user: 'law_book',
+    password: 'law_book',
+    database: 'law_book'
+});
 /* GET users listing. */
 router.get('/', function (req, res) {
     res.send('respond with a resource');
