acca2f2af8b2cd124fc12deb0cbe57b12faf3e75
ludike23 <ludike23@gmail.com>
2020-08-25 02:32:35 +0100

    update json

--- app.js
+++ app.js
@@ -24,7 +24,7 @@ var port = process.env.PORT || 3000;
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 app.use(logger('dev'));
-app.use(bodyParser.json({ limit: 5000 }));
+app.use(bodyParser.json({ limit: 500000 }));
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
